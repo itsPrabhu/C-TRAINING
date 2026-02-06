30cd40b0160f9be5bee2beff853453751865b436
Prabhu <bprabhuram532007@gmail.com>
2026-02-06 11:04:01 +0530

    Create MANUAL POWER CALCULATOR.c

---
+++ MANUAL POWER CALCULATOR.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b,sq;
+    scanf("%d %d",&a,&b);
+    sq=pow(a,b);
+    printf("%d",sq);
+    
+    return 0;
+}
